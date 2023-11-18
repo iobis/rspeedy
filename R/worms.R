b45683f207b16358901e35ea28f82e276b06d227
Pieter Provoost <pieterprovoost@gmail.com>
2023-11-18 21:40:27 +0100

    worms to terra

--- R/worms.R
+++ R/worms.R
@@ -5,52 +5,41 @@ fetch_geom <- function(mrgid) {
   message(glue("Fetching geom for mrgid {mrgid}"))
   url <- glue::glue("https://marineregions.org/rest/getGazetteerGeometries.jsonld/{mrgid}/")
   geom <- jsonlite::fromJSON(url)$"mr:hasGeometry"$"gsp:asWKT"
-  if (is.null(geom)) {
-    return(st_sfc(st_polygon()))
-  } else {
-    geom_fixed <- stringr::str_replace(geom, "<.*>\\s+", "")
-    geom_fixed <- geom_fixed[geom_fixed != ""]
-    geom_union <- st_union(st_as_sfc(geom_fixed))
-    return(geom_union)
+  if (!is.null(geom)) {
+    geom_fixed <- stringr::str_replace(geom, "<.*>\\s+", "") %>%
+    return(geom_fixed)
   }
 }
 
-#' Get geometries by MRGID (cached).
-#'
-#' @export
-fetch_geom_cached <- memoise::memoise(fetch_geom)
-
-#' Get geometries by MRGID.
-#'
-#' @export
-mr_geometries <- function(mrgids) {
-  sapply(mrgids, fetch_geom_cached)
-}
+wm_distribution_possibly <- possibly(wm_distribution, otherwise = NULL)
 
 worms_geoms_by_aphiaid <- function(aphiaid) {
   message(glue("Fetching geoms for aphiaid {aphiaid}"))
   # TODO: buffer non polygon types
-  tryCatch({
-    dist <- wm_distribution(aphiaid) %>%
-      mutate(mrgid = str_replace_all(locationID, "http://marineregions.org/mrgid/", ""))
-    mrgids <- unique(dist$mrgid)
-    geoms <- mr_geometries(mrgids)
-    spatial <- tibble(geom = geoms, mrgid = names(geom)) %>%
-      as.data.frame()
-    dist_all <- dist %>%
-      left_join(spatial, by = "mrgid") %>%
-      st_as_sf(crs = 4326)
-    dist_collection <- dist_all %>% filter(st_is(., "GEOMETRYCOLLECTION")) %>%
-      st_collection_extract()
-    dist_not_collection <- dist_all %>% filter(!st_is(., "GEOMETRYCOLLECTION"))
-    dist <- rbind(dist_collection, dist_not_collection) %>%
-      filter(!st_is(., "MULTIPOINT") & !st_is(., "POINT") & !st_is(., "LINESTRING") & !st_is(., "MULTILINESTRING") & !st_is_empty(.)) %>%
-      st_make_valid()
-    return(dist)
-  }, error = function(err) {
-    message(err$message)
-    empty_sf()
+  dist <- wm_distribution_possibly(aphiaid)
+  if (is.null(dist)) {
+    return(data.frame())
+  }
+  dist <- dist %>%
+    mutate(mrgid = str_replace_all(locationID, "http://marineregions.org/mrgid/", ""))
+  res <- map(1:nrow(dist), function(i) {
+    geoms <- fetch_geom(dist$mrgid[i])
+    if (!is.null(geoms)) {
+      split_geoms <- c()
+      for (j in 1:length(geoms)) {
+        if (str_detect(geoms[j], "GEOMETRYCOLLECTION")) {
+          content <- str_match(geoms[j], "GEOMETRYCOLLECTION\\s?\\((.*)\\)")[,2]
+          parts <- str_extract_all(content, "(?:POLYGON|POINT|LINESTRING)\\s?\\([^A-Z]+\\)")[[1]]
+          split_geoms <- c(split_geoms, parts)
+        } else {
+          split_geoms <- c(split_geoms, geoms[j])
+        }
+      }
+      return(data.frame(wkt = split_geoms, locality = dist$locality[i], establishmentMeans = dist$establishmentMeans[i]))
+    }
   })
+  result <- bind_rows(res)
+  return(result)
 }
 
 #' Get all synonymized AphiaIDs for an AphiaID.
@@ -73,49 +62,46 @@ get_worms_dist <- function(scientificname = NULL, aphiaid = NULL, taxonkey = NUL
   taxonomy <- insistent_resolve_taxonomy(scientificname, aphiaid, taxonkey)
   stopifnot(is.numeric(taxonomy$aphiaid))
   message(glue("Generating dist for aphiaid {aphiaid}"))
-  sf_use_s2(FALSE)
 
   aphiaids <- get_all_aphiaids_by_aphiaid(taxonomy$aphiaid)
-
   dist_map <- map(aphiaids, worms_geoms_by_aphiaid)
   if (sum(sapply(dist_map, nrow)) == 0) {
-    return(empty_sf())
+    return(vect())
   }
   dist_all <- do.call(rbind, dist_map)
+  dist_all <- dist_all[!is.na(dist_all$wkt) & dist_all$wkt != "",]
+  wkt_vect <- map(dist_all$wkt, vect)
+  which_polygons <- which(map(wkt_vect, geomtype) == "polygons")
+  wkt_vect <- wkt_vect[which_polygons]
+  data <- dist_all[which_polygons, c("locality", "establishmentMeans")] %>% mutate(
+    establishmentMeans = case_match(
+      as.character(establishmentMeans),
+      "Alien" ~ "Alien",
+      "Native" ~ "Native",
+      .default = "Other"
+    )
+  )
+  combined <- vect(wkt_vect)
+  values(combined) <- data
+  aggregated <- aggregate(combined, by = "establishmentMeans", dissolve = TRUE)
+  aggregated_buffered <- buffer(aggregated, 0.01)
+  bbox <- vect("POLYGON ((-180 -90, -180 90, 180 90, 180 -90, -180 -90))")
+  aggregated_buffered_cropped <- crop(aggregated_buffered, bbox)
+  aggregated_buffered_cropped_noholes <- fillHoles(aggregated_buffered_cropped)
+  aggregated_buffered_cropped_noholes_simplified <- simplifyGeom(aggregated_buffered_cropped_noholes, 0.001)
+  crs(aggregated_buffered_cropped_noholes_simplified) <- "epsg:4326"
 
-  dist_grouped <- dist_all %>%
-    nngeo::st_remove_holes(max_area = 500000000) %>%
-    mutate(
-      establishmentMeans = case_match(
-        as.character(establishmentMeans),
-        "Alien" ~ "Alien",
-        "Native" ~ "Native",
-        .default = "Other"
-      )
-    ) %>%
-    group_by(establishmentMeans) %>%
-    summarize() %>%
-    nngeo::st_remove_holes(max_area = 500000000)
+  layers <- split(aggregated_buffered_cropped_noholes_simplified, "establishmentMeans")
+  names(layers) <- sapply(layers, function(x) { x$establishmentMeans })
+  layers <- layers[order(names(layers), decreasing = TRUE)]
 
-  dist_simplified <- dist_grouped %>%
-    ms_simplify(keep_shapes = TRUE, snap = FALSE) %>%
-    st_make_valid()
-
-  dist_alien <- dist_simplified %>%
-    filter(establishmentMeans == "Alien")
-  dist_native <- dist_simplified %>%
-    filter(establishmentMeans == "Native")
-  dist_other <- dist_simplified %>%
-    filter(establishmentMeans == "Other")
-  if (nrow(dist_alien) > 0) {
-    dist_other <- dist_other %>% st_difference(st_geometry(dist_alien))
-    dist_native <- dist_native %>% st_difference(st_geometry(dist_alien))
+  if (length(layers) > 1) {
+    for (i in 2:length(layers)) {
+      layers[[1]] <- cover(layers[[1]], layers[[i]])
+    }
   }
-  if (nrow(dist_native) > 0) {
-    dist_other <- dist_other %>% st_difference(st_geometry(dist_native))
-  }
 
-  return(bind_rows(dist_alien, dist_native, dist_other))
+  return(layers[[1]])
 }
 
 #' Get name for AphiaID.
